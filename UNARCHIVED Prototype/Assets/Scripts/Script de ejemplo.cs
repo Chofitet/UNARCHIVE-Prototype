232fde3a0f759f1231efcad23514461a7789770c
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2022-04-30 16:38:11 -0300

    Hola Mundo

--- UNARCHIVED Prototype/Assets/Scripts/Script de ejemplo.cs
+++ UNARCHIVED Prototype/Assets/Scripts/Script de ejemplo.cs
@@ -7,7 +7,7 @@ public class Scriptdeejemplo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola mundo");
     }
 
     // Update is called once per frame
